02528b4f44119c2af1f559d193835c0143db262d
ZeroForgotten <ty44679@gmail.com>
2025-10-30 10:05:51 -0400

    more bread and goose

---
+++ src/NPCs/EvilGoose.java
@@ -0,0 +1,71 @@
+package NPCs;
+
+import Builders.FrameBuilder;
+import Engine.GraphicsHandler;
+import Engine.ImageLoader;
+import GameObject.Frame;
+import GameObject.ImageEffect;
+import GameObject.SpriteSheet;
+import Level.NPC;
+import Level.Player;
+import Utils.Point;
+import java.util.HashMap;
+
+// This class is for the BREADPC
+public class EvilGoose extends NPC {
+
+    public EvilGoose(Point location) {
+        super(location.x, location.y, new SpriteSheet(ImageLoader.load("EvilGoose.png"), 24, 24), "Goose");
+        isInteractable = true;
+
+        // Get portrait frames
+        Frame npcPortrait = getPortraitFrame();
+        SpriteSheet playerSheet = new SpriteSheet(ImageLoader.load("Cat.png"), 24, 24);
+        Frame playerPortrait = new Builders.FrameBuilder(playerSheet.getSprite(0, 0))
+                .withScale(3)
+                .withBounds(3, 3, 17, 19)
+                .build();
+
+        // Setup dialogue sequence
+        dialogueSequence = new Level.DialogueSequence();
+        dialogueSequence.addMessage("You have forsaken us brother.", npcPortrait);
+        dialogueSequence.addMessage("It is I how is the choosen one.", playerPortrait);
+        dialogueSequence.addMessage("Are breadtheren will come for you.", npcPortrait);
+        dialogueSequence.addMessage("*Smug*", playerPortrait);
+    }
+
+    public void update(Player player) {
+        // while npc is being talked to, it frowns
+        if (isInDialogue) {
+            currentAnimationName = "Goose";
+        } else {
+            currentAnimationName = "Goose1";
+        }
+
+        super.update(player);
+    }
+
+    @Override
+    public HashMap<String, Frame[]> loadAnimations(SpriteSheet spriteSheet) {
+        return new HashMap<String, Frame[]>() {{
+           put("Smile", new Frame[] {
+                   new FrameBuilder(spriteSheet.getSprite(0, 0))
+                           .withScale(4)
+                           .withImageEffect(ImageEffect.FLIP_HORIZONTAL)
+                           .build()
+           });
+            put("Frown", new Frame[] {
+                    new FrameBuilder(spriteSheet.getSprite(1, 0))
+                            .withScale(4)
+                            .withImageEffect(ImageEffect.FLIP_HORIZONTAL)
+                            .build()
+            });
+        }};
+    }
+
+    @Override
+    public void draw(GraphicsHandler graphicsHandler) {
+        super.draw(graphicsHandler);
+    }
+}
+
