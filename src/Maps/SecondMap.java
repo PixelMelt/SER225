a4e2af123e3607d57a917212872b6f304fde7c3d
pixelmelt <derptoday371@gmail.com>
2025-11-13 19:19:53 -0500

    make tileset not annoying

--- src/Maps/SecondMap.java
+++ src/Maps/SecondMap.java
@@ -50,33 +50,14 @@ public class SecondMap extends Map {
         enhancedMapTiles.add(vmp);
 
         // add spikes
-        for (int i = 9; i < 27; i++) {
-            Spike spikeG = new Spike(getMapTile(i, 17).getLocation());
-            enhancedMapTiles.add(spikeG);
-        }
-
-        for (int i = 31; i <= 48; i++) {
-            MapTile t = getMapTile(i, 17);
-            if (t != null && t.getTileType() == TileType.PASSABLE) {
-                Spike spikeG = new Spike(t.getLocation());
-                enhancedMapTiles.add(spikeG);
-            }
-        }
-
-        for (int i = 81; i <= 94; i++) {
-            MapTile t = getMapTile(i, 17);
-            if (t != null && t.getTileType() == TileType.PASSABLE) {
-                Spike spikeG = new Spike(t.getLocation());
-                enhancedMapTiles.add(spikeG);
-            }
-        }
-
-        int[] spikeXs = {58, 59, 62, 63, 64, 65, 68, 69};
-        for (int x : spikeXs) {
-            MapTile t = getMapTile(x, 9);
-            if (t != null && t.getTileType() == TileType.PASSABLE) {
-                Spike spikeG = new Spike(t.getLocation());
-                enhancedMapTiles.add(spikeG);
+        for (int y = 0; y < getHeight(); y++) {
+            for (int x = 0; x < getWidth(); x++) {
+                MapTile tile = getMapTile(x, y);
+                if (tile != null && tile.getTileIndex() == 16) {
+                    enhancedMapTiles.add(new Spike(tile.getLocation()));
+                    // replace tile 16 with tile -1 to hide it from rendering
+                    setTileIndex(x, y, -1);
+                }
             }
         }
 
@@ -84,94 +65,19 @@ public class SecondMap extends Map {
         enhancedMapTiles.add(endLevelBox);
 
         // add buzzsaws
-        MapTile buzz1 = getMapTile(11, 10);
-        if (buzz1 != null && buzz1.getTileType() == TileType.PASSABLE) {
-            enhancedMapTiles.add(new Buzzsaw(buzz1.getLocation()));
-        }
-
-        MapTile buzz2 = getMapTile(15, 10);
-        if (buzz2 != null && buzz2.getTileType() == TileType.PASSABLE) {
-            enhancedMapTiles.add(new Buzzsaw(buzz2.getLocation()));
-        }
-
-        MapTile buzz3 = getMapTile(19, 10);
-        if (buzz3 != null && buzz3.getTileType() == TileType.PASSABLE) {
-            enhancedMapTiles.add(new Buzzsaw(buzz3.getLocation()));
-        }
-
-        MapTile buzz4 = getMapTile(23, 10);
-        if (buzz4 != null && buzz4.getTileType() == TileType.PASSABLE) {
-            enhancedMapTiles.add(new Buzzsaw(buzz4.getLocation()));
-        }
-
-        MapTile buzz5 = getMapTile(27, 10);
-        if (buzz5 != null && buzz5.getTileType() == TileType.PASSABLE) {
-            enhancedMapTiles.add(new Buzzsaw(buzz5.getLocation()));
-        }
-
-        MapTile buzz6 = getMapTile(13, 13);
-        if (buzz6 != null && buzz6.getTileType() == TileType.PASSABLE) {
-            enhancedMapTiles.add(new Buzzsaw(buzz6.getLocation()));
-        }
-
-        MapTile buzz7 = getMapTile(17, 13);
-        if (buzz7 != null && buzz7.getTileType() == TileType.PASSABLE) {
-            enhancedMapTiles.add(new Buzzsaw(buzz7.getLocation()));
-        }
-
-        MapTile buzz8 = getMapTile(21, 13);
-        if (buzz8 != null && buzz8.getTileType() == TileType.PASSABLE) {
-            enhancedMapTiles.add(new Buzzsaw(buzz8.getLocation()));
-        }
-
-        MapTile buzz9 = getMapTile(25, 13);
-        if (buzz9 != null && buzz9.getTileType() == TileType.PASSABLE) {
-            enhancedMapTiles.add(new Buzzsaw(buzz9.getLocation()));
-        }
-
-        MapTile buzz10 = getMapTile(34, 10);
-        if (buzz10 != null && buzz10.getTileType() == TileType.PASSABLE) {
-            enhancedMapTiles.add(new Buzzsaw(buzz10.getLocation()));
-        }
-
-        MapTile buzz11 = getMapTile(44, 10);
-        if (buzz11 != null && buzz11.getTileType() == TileType.PASSABLE) {
-            enhancedMapTiles.add(new Buzzsaw(buzz11.getLocation()));
-        }
-
-        MapTile buzz12 = getMapTile(39, 6);
-        if (buzz12 != null && buzz12.getTileType() == TileType.PASSABLE) {
-            enhancedMapTiles.add(new Buzzsaw(buzz12.getLocation()));
-        }
-
-        MapTile buzz13 = getMapTile(71, 9);
-        if (buzz13 != null && buzz13.getTileType() == TileType.PASSABLE) {
-            enhancedMapTiles.add(new Buzzsaw(buzz13.getLocation()));
-        }
-
-        MapTile buzz14 = getMapTile(74, 12);
-        if (buzz14 != null && buzz14.getTileType() == TileType.PASSABLE) {
-            enhancedMapTiles.add(new Buzzsaw(buzz14.getLocation()));
-        }
-
-        MapTile buzz15 = getMapTile(77, 15);
-        if (buzz15 != null && buzz15.getTileType() == TileType.PASSABLE) {
-            enhancedMapTiles.add(new Buzzsaw(buzz15.getLocation()));
-        }
-
-        MapTile buzz16 = getMapTile(51, 14);
-        if (buzz16 != null && buzz16.getTileType() == TileType.PASSABLE) {
-            enhancedMapTiles.add(new Buzzsaw(buzz16.getLocation()));
-        }
-
-        MapTile buzz17 = getMapTile(55, 10);
-        if (buzz17 != null && buzz17.getTileType() == TileType.PASSABLE) {
-            enhancedMapTiles.add(new Buzzsaw(buzz17.getLocation()));
+        for (int y = 0; y < getHeight(); y++) {
+            for (int x = 0; x < getWidth(); x++) {
+                MapTile tile = getMapTile(x, y);
+                if (tile != null && tile.getTileIndex() == 15) {
+                    enhancedMapTiles.add(new Buzzsaw(tile.getLocation()));
+                    // replace tile 15 with tile -1 to hide it from rendering
+                    setTileIndex(x, y, -1);
+                }
+            }
         }
 
         return enhancedMapTiles;
     }
-
     
     @Override
     public ArrayList<NPC> loadNPCs() {
@@ -182,6 +88,4 @@ public class SecondMap extends Map {
 
         return npcs;
     }
-    
-
 }
