26bacdaaaa91ce910e45b37cdd23e704a5ad0816
JULEZ4040 <jgcolon00@gmail.com>
2025-11-18 16:55:27 -0500

    level 9 and stuff you're welcome PixleMelt.A A A A

--- src/Maps/NinthMap.java
+++ src/Maps/NinthMap.java
@@ -1,5 +1,7 @@
 package Maps;
 
+import EnhancedMapTiles.BreakBox;
+import EnhancedMapTiles.Buzzsaw;
 import EnhancedMapTiles.EndLevelBox;
 import EnhancedMapTiles.Spike;
 import Level.*;
@@ -7,21 +9,62 @@ import NPCs.BreadGoose;
 import Tilesets.CommonTileset;
 import java.util.ArrayList;
 
+import Engine.Config;
+
 // Represents the map that is used as a background for the main menu and credits menu screen
 public class NinthMap extends Map {
 
     public NinthMap() {
         super("ninth_map.txt", new CommonTileset());
-        //this.playerStartPosition = getMapTile(3, 47).getLocation();
+        this.playerStartPosition = getMapTile(0, 42).getLocation();
     }
 
      @Override
     public ArrayList<EnhancedMapTile> loadEnhancedMapTiles() {
         ArrayList<EnhancedMapTile> enhancedMapTiles = new ArrayList<>();
 
-        //EndLevelBox endLevelBox = new EndLevelBox(getMapTile(246, 43).getLocation());
-        //enhancedMapTiles.add(endLevelBox);
-        
+        EndLevelBox endLevelBox = new EndLevelBox(getMapTile(92, 46).getLocation());
+        enhancedMapTiles.add(endLevelBox);
+    
+        // add spikes
+        for (int y = 0; y < getHeight(); y++) {
+            for (int x = 0; x < getWidth(); x++) {
+                MapTile tile = getMapTile(x, y);
+                if (tile != null && tile.getTileIndex() == 16) {
+                    enhancedMapTiles.add(new Spike(tile.getLocation()));
+                    if (!Config.IS_EDITOR_MODE) {
+                        setTileIndex(x, y, -1);
+                    }
+                }
+            }
+        }
+
+        // add buzzsaws
+        for (int y = 0; y < getHeight(); y++) {
+            for (int x = 0; x < getWidth(); x++) {
+                MapTile tile = getMapTile(x, y);
+                if (tile != null && tile.getTileIndex() == 15) {
+                    enhancedMapTiles.add(new Buzzsaw(tile.getLocation()));
+                    if (!Config.IS_EDITOR_MODE) {
+                        setTileIndex(x, y, -1);
+                    }
+                }
+            }
+        }
+
+        // add breakboxes
+        for (int y = 0; y < getHeight(); y++) {
+            for (int x = 0; x < getWidth(); x++) {
+                MapTile tile = getMapTile(x, y);
+                if (tile != null && tile.getTileIndex() == 14) {
+                    enhancedMapTiles.add(new BreakBox(tile.getLocation()));
+                    if (!Config.IS_EDITOR_MODE) {
+                        setTileIndex(x, y, -1);
+                    }
+                }
+            }
+        }
+   
         return enhancedMapTiles;
 
     }
