0a2a8dd67921d231f9ceb06e86f39c9365b9dded
Alex4617 <adpina@quinnipiac.edu>
2025-11-19 17:31:41 -0500

    level tuning

--- src/Maps/FourthMap.java
+++ src/Maps/FourthMap.java
@@ -1,11 +1,15 @@
 package Maps;
 
+import EnhancedMapTiles.BreakBox;
+import EnhancedMapTiles.Buzzsaw;
 import EnhancedMapTiles.EndLevelBox;
 import EnhancedMapTiles.Spike;
 import Level.*;
 import NPCs.BreadPC;
 import Tilesets.CommonTileset;
 import java.util.ArrayList;
+
+import Engine.Config;
 
 // Represents a test map to be used in a level
 public class FourthMap extends Map {
@@ -22,90 +26,43 @@ public class FourthMap extends Map {
         EndLevelBox endLevelBox = new EndLevelBox(getMapTile(48, 2).getLocation());
         enhancedMapTiles.add(endLevelBox);
 
-        //Spikes
-
-        int array0[] = {8,9,10,20,21,22};
-        for(int i: array0){
-            Spike spikesFloor = new Spike(getMapTile(i,72).getLocation()); 
-            enhancedMapTiles.add(spikesFloor);
+        // add spikes
+        for (int y = 0; y < getHeight(); y++) {
+            for (int x = 0; x < getWidth(); x++) {
+                MapTile tile = getMapTile(x, y);
+                if (tile != null && tile.getTileIndex() == 16) {
+                    enhancedMapTiles.add(new Spike(tile.getLocation()));
+                    if (!Config.IS_EDITOR_MODE) {
+                        setTileIndex(x, y, -1);
+                    }
+                }
+            }
         }
 
-        int array[] = {39,43,63};
-        for(int i: array){
-            Spike spikes0 = new Spike(getMapTile(0,i).getLocation().addY(20)); 
-            enhancedMapTiles.add(spikes0);
+        // add buzzsaws
+        for (int y = 0; y < getHeight(); y++) {
+            for (int x = 0; x < getWidth(); x++) {
+                MapTile tile = getMapTile(x, y);
+                if (tile != null && tile.getTileIndex() == 15) {
+                    enhancedMapTiles.add(new Buzzsaw(tile.getLocation()));
+                    if (!Config.IS_EDITOR_MODE) {
+                        setTileIndex(x, y, -1);
+                    }
+                }
+            }
         }
 
-        Spike spikes1 = new Spike(getMapTile(1,43).getLocation().addY(20)); 
-        enhancedMapTiles.add(spikes1);
-
-        int array1[] = {27,51,67};
-        for(int i: array1){
-            Spike spikes2 = new Spike(getMapTile(2,i).getLocation().addY(20)); 
-            enhancedMapTiles.add(spikes2);
-        }
-
-        Spike spikes4 = new Spike(getMapTile(4,55).getLocation().addY(20)); 
-        enhancedMapTiles.add(spikes4);
-
-        Spike spikes8 = new Spike(getMapTile(8,51).getLocation().addY(20)); 
-        enhancedMapTiles.add(spikes8);
-
-        Spike spikes9 = new Spike(getMapTile(9,35).getLocation().addY(20)); 
-        enhancedMapTiles.add(spikes9);
-
-        int array2[] = {35,65};
-        for(int i: array2){
-            Spike spikes10 = new Spike(getMapTile(10,i).getLocation().addY(20)); 
-            enhancedMapTiles.add(spikes10);
-        }
-
-        int array3[] = {35,51,65};
-        for(int i: array3){
-            Spike spikes11 = new Spike(getMapTile(11,i).getLocation().addY(20)); 
-            enhancedMapTiles.add(spikes11);
-        }
-
-        int array4[] = {35,51,65};
-        for(int i: array4){
-            Spike spikes12 = new Spike(getMapTile(12,i).getLocation().addY(20)); 
-            enhancedMapTiles.add(spikes12);
-        }
-
-        Spike spikes18 = new Spike(getMapTile(18,43).getLocation().addY(20)); 
-        enhancedMapTiles.add(spikes18);
-
-        int array5[] = {31,43};
-        for(int i: array5){
-            Spike spikes19 = new Spike(getMapTile(19,i).getLocation().addY(20)); 
-            enhancedMapTiles.add(spikes19);
-        }
-
-        Spike spikes20 = new Spike(getMapTile(20,47).getLocation().addY(20)); 
-        enhancedMapTiles.add(spikes20);
-
-        Spike spikes22 = new Spike(getMapTile(22,39).getLocation().addY(20)); 
-        enhancedMapTiles.add(spikes22);
-
-        int array6[] = {26,30};
-        for(int i: array6){
-            Spike spikes42 = new Spike(getMapTile(42,i).getLocation().addY(20)); 
-            enhancedMapTiles.add(spikes42);
-        }
-
-        Spike spikes42b = new Spike(getMapTile(42,63).getLocation()); 
-        enhancedMapTiles.add(spikes42b);
-
-        Spike spikes43b = new Spike(getMapTile(43,64).getLocation()); 
-        enhancedMapTiles.add(spikes43b);
-
-        Spike spikes43 = new Spike(getMapTile(43,30).getLocation().addY(20)); 
-        enhancedMapTiles.add(spikes43);
-
-        int array8[] = {18,46,57};
-        for(int i: array8){
-            Spike spikes46 = new Spike(getMapTile(46,i).getLocation().addY(20)); 
-            enhancedMapTiles.add(spikes46);
+        // add breakboxes
+        for (int y = 0; y < getHeight(); y++) {
+            for (int x = 0; x < getWidth(); x++) {
+                MapTile tile = getMapTile(x, y);
+                if (tile != null && tile.getTileIndex() == 14) {
+                    enhancedMapTiles.add(new BreakBox(tile.getLocation()));
+                    if (!Config.IS_EDITOR_MODE) {
+                        setTileIndex(x, y, -1);
+                    }
+                }
+            }
         }
 
         return enhancedMapTiles;
--- src/Maps/ThirdMap.java
+++ src/Maps/ThirdMap.java
@@ -1,11 +1,15 @@
 package Maps;
 
+import EnhancedMapTiles.BreakBox;
 import EnhancedMapTiles.Buzzsaw;
 import EnhancedMapTiles.EndLevelBox;
+import EnhancedMapTiles.Spike;
 import Level.*;
 import NPCs.Duck;
 import Tilesets.CommonTileset;
 import java.util.ArrayList;
+
+import Engine.Config;
 
 // Represents a test map to be used in a level
 public class ThirdMap extends Map {
@@ -19,124 +23,42 @@ public class ThirdMap extends Map {
     public ArrayList<EnhancedMapTile> loadEnhancedMapTiles() {
         ArrayList<EnhancedMapTile> enhancedMapTiles = new ArrayList<>();
 
-        for(int i = 7; i < 24; i++){
-            Buzzsaw buzz1 = new Buzzsaw(getMapTile(i, 28).getLocation());
-            enhancedMapTiles.add(buzz1);
-        }
-
-        for(int i = 76; i < 92; i++){
-            Buzzsaw buzz1 = new Buzzsaw(getMapTile(i, 28).getLocation());
-            enhancedMapTiles.add(buzz1);
-        }
-
-        //Saw Maze Hell
-        Buzzsaw buzz20 = new Buzzsaw(getMapTile(47, 13).getLocation());
-        enhancedMapTiles.add(buzz20);
-
-        Buzzsaw buzz21 = new Buzzsaw(getMapTile(62, 15).getLocation());
-        enhancedMapTiles.add(buzz21);
-
-        Buzzsaw buzz22 = new Buzzsaw(getMapTile(69, 17).getLocation());
-        enhancedMapTiles.add(buzz22);
-
-        for(int i = 3; i < 24; i++){
-            Buzzsaw buzz1 = new Buzzsaw(getMapTile(43, i).getLocation());
-            enhancedMapTiles.add(buzz1);
-        }
-
-        for(int i = 63; i < 71; i++){
-            Buzzsaw buzz1 = new Buzzsaw(getMapTile(i, 12).getLocation());
-            enhancedMapTiles.add(buzz1);
-        }
-
-        for(int i = 17; i < 24; i++){
-            Buzzsaw buzz1 = new Buzzsaw(getMapTile(44, i).getLocation());
-            enhancedMapTiles.add(buzz1);
-        }
-
-        for(int i = 44; i < 61; i++){
-            Buzzsaw buzz1 = new Buzzsaw(getMapTile(i, 3).getLocation());
-            enhancedMapTiles.add(buzz1);
-        }
-
-        for(int i = 6; i < 15; i++){
-            Buzzsaw buzz1 = new Buzzsaw(getMapTile(62, i).getLocation());
-            enhancedMapTiles.add(buzz1);
-        }
-
-        for(int i = 17; i < 27; i++){
-            Buzzsaw buzz1 = new Buzzsaw(getMapTile(66, i).getLocation());
-            enhancedMapTiles.add(buzz1);
-        }
-
-        for(int i = 21; i < 27; i++){
-            Buzzsaw buzz1 = new Buzzsaw(getMapTile(67, i).getLocation());
-            enhancedMapTiles.add(buzz1);
-        }
-
-        for(int i = 13; i < 24; i++){
-            Buzzsaw buzz1 = new Buzzsaw(getMapTile(70, i).getLocation());
-            enhancedMapTiles.add(buzz1);
-        }
-
-        for(int i = 16; i < 27; i++){
-            Buzzsaw buzz1 = new Buzzsaw(getMapTile(54, i).getLocation());
-            enhancedMapTiles.add(buzz1);
-        }
-
-        for(int i = 0; i < 2; i++){
-            Buzzsaw buzz1 = new Buzzsaw(getMapTile(50+i, 9).getLocation());
-            enhancedMapTiles.add(buzz1);
-            Buzzsaw buzz2 = new Buzzsaw(getMapTile(54+i, 9).getLocation());
-            enhancedMapTiles.add(buzz2);
-            Buzzsaw buzz3 = new Buzzsaw(getMapTile(58+i, 9).getLocation());
-            enhancedMapTiles.add(buzz3);
-            Buzzsaw buzz4 = new Buzzsaw(getMapTile(58+i, 15).getLocation());
-            enhancedMapTiles.add(buzz4);
-        }
-
-        for(int i = 59; i < 63; i++){
-            for(int j = 16; j < 24; j++){
-                Buzzsaw buzz1 = new Buzzsaw(getMapTile(i, j).getLocation());
-                enhancedMapTiles.add(buzz1);
+        // add spikes
+        for (int y = 0; y < getHeight(); y++) {
+            for (int x = 0; x < getWidth(); x++) {
+                MapTile tile = getMapTile(x, y);
+                if (tile != null && tile.getTileIndex() == 16) {
+                    enhancedMapTiles.add(new Spike(tile.getLocation()));
+                    if (!Config.IS_EDITOR_MODE) {
+                        setTileIndex(x, y, -1);
+                    }
+                }
             }
         }
 
-        for(int i = 55; i < 57; i++){
-            for(int j = 19; j < 27; j++){
-                Buzzsaw buzz1 = new Buzzsaw(getMapTile(i, j).getLocation());
-                enhancedMapTiles.add(buzz1);
+        // add buzzsaws
+        for (int y = 0; y < getHeight(); y++) {
+            for (int x = 0; x < getWidth(); x++) {
+                MapTile tile = getMapTile(x, y);
+                if (tile != null && tile.getTileIndex() == 15) {
+                    enhancedMapTiles.add(new Buzzsaw(tile.getLocation()));
+                    if (!Config.IS_EDITOR_MODE) {
+                        setTileIndex(x, y, -1);
+                    }
+                }
             }
         }
 
-        for(int i = 45; i < 51; i++){
-            for(int j = 20; j < 24; j++){
-                Buzzsaw buzz1 = new Buzzsaw(getMapTile(i, j).getLocation());
-                enhancedMapTiles.add(buzz1);
-            }
-        }
-
-        for(int i = 61; i < 63; i++){
-            for(int j = 3; j < 6; j++){
-                Buzzsaw buzz1 = new Buzzsaw(getMapTile(i, j).getLocation());
-                enhancedMapTiles.add(buzz1);
-            }
-        }
-
-        for(int i = 48; i < 60; i++){
-            Buzzsaw buzz1 = new Buzzsaw(getMapTile(i, 10).getLocation());
-            enhancedMapTiles.add(buzz1);
-        }
-
-        for(int i = 48; i < 57; i++){
-            Buzzsaw buzz1 = new Buzzsaw(getMapTile(i, 11).getLocation());
-            enhancedMapTiles.add(buzz1);
-        }
-
-        for(int i = 48; i < 55; i++){
-            for(int j = 12; j < 16; j++){
-                Buzzsaw buzz1 = new Buzzsaw(getMapTile(i, j).getLocation());
-                enhancedMapTiles.add(buzz1);
+        // add breakboxes
+        for (int y = 0; y < getHeight(); y++) {
+            for (int x = 0; x < getWidth(); x++) {
+                MapTile tile = getMapTile(x, y);
+                if (tile != null && tile.getTileIndex() == 14) {
+                    enhancedMapTiles.add(new BreakBox(tile.getLocation()));
+                    if (!Config.IS_EDITOR_MODE) {
+                        setTileIndex(x, y, -1);
+                    }
+                }
             }
         }
 
